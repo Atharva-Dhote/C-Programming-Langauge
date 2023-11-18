e749dd2042e102825d7cc22f6f09e15c701bd764
Atharva-Dhote <143984230+Atharva-Dhote@users.noreply.github.com>
2023-11-18 18:47:11 +0530

    In-Built Operations Of Math.h

---
+++ In Built Operations Of math.h
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <math.h> 
+int main() 
+{
+printf("Name:-Atharva Dhote\n"); 
+printf("Roll No.:-31\n\n"); 
+double x = 2.696900; 
+double y = 3.966900;
+printf("sin(%f) = %f\n", x, sin(x)); 
+printf("cos(%f) = %f\n", x, cos(x)); 
+printf("tan(%f) = %f\n", x, tan(x)); 
+printf("exp(%f) = %f\n", x, exp(x)); 
+printf("log(%f) = %f\n", x, log(x)); 
+printf("log10(%f) = %f\n", x, log10(x)); 
+printf("pow(%f, %f) = %f\n", x, y, pow(x, y)); 
+printf("sqrt(%f) = %f\n", x, sqrt(x));
+printf("cbrt(%f) = %f\n",cbrt(x)); 
+return 0;
+} 
